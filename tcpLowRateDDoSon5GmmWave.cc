eefad04a8daf44facba35d01d66feb31d88b67e1
yuripton <prihantono.yuri@gmail.com>
2020-11-10 09:49:45 +0700

    Update tcpLowRateDDoSon5GmmWave.cc
    update topology

--- tcpLowRateDDoSon5GmmWave.cc
+++ tcpLowRateDDoSon5GmmWave.cc
@@ -1,5 +1,5 @@
 /* 
- * The topology used to simulate this attack contains 4 nodes as follows:
+ * The topology used to simulate this attack contains 7 nodes as follows:
  * n0 -> arini (UE legitimate User)
  * n1 -> endro (4 UE attackers)
  * n2 -> enb (base station connected to yuri)
@@ -14,7 +14,7 @@
          ////    
         ////
        //// pp1 
-     n1
+     a1a2a3a4
 */
 
 #include "ns3/nstime.h"
@@ -22,4 +22,15 @@
 #include "ns3/network-module.h"
 #include "ns3/internet-module.h"
 #include "ns3/point-to-point-module.h"
+#include "ns3/mmwave-helper.h"
+#include "ns3/applications-module.h"
 
+
+//Main function
+int main (int argc, char *argv[])
+{
+  
+  Simulator::Run();
+  Simulator::Destroy();
+  return 0;
+}
